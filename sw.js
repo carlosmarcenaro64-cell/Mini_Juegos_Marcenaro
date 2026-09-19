939fd76c214a3c114a322885ad0f5432e8616328
carlosmarcenaro64-cell <carlosmarcenaro64@gmail.com>
2026-09-19 17:35:54 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='mini-juegos-v1';
+const CORE=['./','./index.html','./index_palabra_secreta.html','./index_en.html','./index_temp.html','./pastel.html','./ranking.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE).catch(()=>{}))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+});
